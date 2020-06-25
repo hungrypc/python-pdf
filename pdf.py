be2c8e016c84555235a87616f8768e4962b9f9ce
hpc <philipchanhk626@gmail.com>
2020-06-25 08:53:36 -0400

    pdf_combiner

--- pdf.py
+++ pdf.py
@@ -1,14 +1,28 @@
 import PyPDF2
+import sys
 
-with open('dummy.pdf', 'rb') as file:
-    # 'rb' converts file object to binary mode so PyPDF2 can read
-    reader = PyPDF2.PdfFileReader(file)
-    print(reader.numPages)      # number of pages
-    print(reader.getPage(0))    # get specific page object
-    page = reader.getPage(0)
-    # page.rotateClockwise(90)
-    page.rotateCounterClockwise(90)
-    writer = PyPDF2.PdfFileWriter()
-    writer.addPage(page)
-    with open('rotated.pdf', 'wb') as new_file:
-        writer.write(new_file)
+inputs = sys.argv[1:]
+
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        print(pdf)
+        merger.append(pdf)
+    merger.write('super.pdf')
+
+
+pdf_combiner(inputs)
+
+# with open('dummy.pdf', 'rb') as file:
+#     # 'rb' converts file object to binary mode so PyPDF2 can read
+#     reader = PyPDF2.PdfFileReader(file)
+#     print(reader.numPages)      # number of pages
+#     print(reader.getPage(0))    # get specific page object
+#     page = reader.getPage(0)
+#     # page.rotateClockwise(90)
+#     page.rotateCounterClockwise(90)
+#     writer = PyPDF2.PdfFileWriter()
+#     writer.addPage(page)
+#     with open('rotated.pdf', 'wb') as new_file:
+#         writer.write(new_file)
